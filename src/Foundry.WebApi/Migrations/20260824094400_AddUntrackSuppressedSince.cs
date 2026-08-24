af30a3151eda1d2019e6098a50c5b7c9a3f2e2fe
Kari Rafn Karlsson <kari.rafn.karlsson@efla.is>
2026-08-24 09:44:54 +0000

    chore(monitoring): add migration for untrack_suppressed_since column

---
+++ src/Foundry.WebApi/Migrations/20260824094400_AddUntrackSuppressedSince.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Foundry.WebApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddUntrackSuppressedSince : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTimeOffset>(
+                name: "untrack_suppressed_since",
+                table: "monitored_repositories",
+                type: "TEXT",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "untrack_suppressed_since",
+                table: "monitored_repositories");
+        }
+    }
+}
